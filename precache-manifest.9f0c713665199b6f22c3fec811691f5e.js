d226c206a9eba3af4b59b4323eb8c7f0b31eb43c
betsy vidal <betsyvidal@MacBook-Pro-de-betsy.local>
2019-01-02 23:24:45 -0500

    Updates

---
+++ precache-manifest.9f0c713665199b6f22c3fec811691f5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "217b8aa3cc0b62b87d190ab64008ba06",
+    "url": "%20//betsyvies.github.io/Card-Game/static/media/trophy.217b8aa3.gif"
+  },
+  {
+    "revision": "42dc521faf8fa9485286ace0551cfa2a",
+    "url": "%20//betsyvies.github.io/Card-Game/static/media/cards.42dc521f.svg"
+  },
+  {
+    "revision": "6fab3125ff5d5ea6a027",
+    "url": "%20//betsyvies.github.io/Card-Game/static/js/runtime~main.6fab3125.js"
+  },
+  {
+    "revision": "b3f0d306abc8c12b7eb7",
+    "url": "%20//betsyvies.github.io/Card-Game/static/js/main.b3f0d306.chunk.js"
+  },
+  {
+    "revision": "f6cae36f608bc63ca716",
+    "url": "%20//betsyvies.github.io/Card-Game/static/js/1.f6cae36f.chunk.js"
+  },
+  {
+    "revision": "b3f0d306abc8c12b7eb7",
+    "url": "%20//betsyvies.github.io/Card-Game/static/css/main.3a6f3663.chunk.css"
+  },
+  {
+    "revision": "fb5874f249b18eb58c10da97d26a4974",
+    "url": "%20//betsyvies.github.io/Card-Game/index.html"
+  }
+];
